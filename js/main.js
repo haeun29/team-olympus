79672e17e02a12e7be7c4c2384b3d035dc2e922f
han <105402303+haeun29@users.noreply.github.com>
2022-08-11 14:58:47 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
+$(function () {
 
-  var swiper = new Swiper(".mySwiper", {
+  $(function () {
+    /** 2가지 유형
+     * 1. 현재 하위 뎁스만 보임
+     * 2. 전체 하위뎁스 표시
+     */
+    $("#gnb .depth1 > li").hover(
+      // mouseover
+      function () {
+        $(".depth2") // 여기만 수정하면 됨 this or 선택자
+          .addClass("on");
+      },
+      // mouseout
+      function () {
+        $(".depth2").removeClass("on");
+      }
+    );
+  });
+  
+
+  $('#family-site-btn').click(
+    function () {
+      $('.family-site-nav > ul')
+        .toggleClass('on')
+    }
+  );
+
+  $("#toggle-btn").click(function(){
+		$("#gnb").addClass("on");
+	});
+
+  $("#close-btn").click(function(){
+		$("#gnb").removeClass("on");
+	});
+
+
+
+}); // $
+  
+   
+  
+  
+ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
